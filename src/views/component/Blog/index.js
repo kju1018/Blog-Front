bd40956efc4a023411e63bb3a42a97305a45c9dd
KIM M S <61860897+kju1018@users.noreply.github.com>
2023-02-09 20:47:37 +0900

    임시커밋

---
+++ src/views/component/Blog/index.js
@@ -0,0 +1,15 @@
+import { useParams } from "react-router-dom";
+
+function Blog(props) {
+
+  // const params = useParams();
+  // console.log(params);
+
+    return (
+      <div>
+        블로그 페이지
+      </div>
+    )
+}
+
+export default Blog;
